3811ee90318d69651c5133dbfd4fd815a4338a77
RomelSikdar <romels500@gmail.com>
2023-10-26 01:50:58 +0530

    error-component addition

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,10 @@ export default defineConfig({
         target: "http://localhost:3002/",
         changeOrigin: true,
       },
+      "/errors": {
+        target: "http://localhost:3003/",
+        changeOrigin: true,
+      },
     },
   },
 });
